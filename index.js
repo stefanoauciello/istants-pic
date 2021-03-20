9055c7ed7d71022cf2996ae016104dcffff03d14
auciellostefano1@gmail.com <auciellostefano1@gmail.com>
2021-03-20 17:33:32 +0100

    add simple query to database on select

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 const express = require('express')
+const mysql = require('mysql');
 const app = express()
 
+const con = mysql.createConnection({
+    host: "127.0.0.1",
+    port: 3306,
+    user: "instantuser",
+    password: "instantpassword"
+});
+
 app.get('/photos', function (req, res) {
+    const sql = 'SELECT ID, NAME, WEIGHT, LENGTH, LATITUDE, LONGITUDE, USERNAME, UPDATED_AT, CREATED_AT FROM instant.PHOTOS;';
+    con.connect(function(err) {
+        if (err) throw err;
+        console.log("Connected!");
+        con.query(sql, function (err, result) {
+            if (err) throw err;
+            console.log("Result: " + JSON.stringify(result));
+        })
+    });
     res.status(200).send('HI');
 })
 
