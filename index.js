01cce5f7a33746b04b4b20c4623c7ba24ec6cb4d
auciellostefano1@gmail.com <auciellostefano1@gmail.com>
2021-03-20 19:21:02 +0100

    add validation, refactor res

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const mysql = require('mysql2');
 const bodyParser = require('body-parser');
+const {body, validationResult} = require('express-validator');
 const app = express();
 
 const jsonParser = bodyParser.json()
-const urlencodedParser = bodyParser.urlencoded({ extended: false })
 app.listen(3000)
+console.log("listening at 3000")
 
 const config = {
     host: "127.0.0.1",
@@ -18,13 +19,31 @@ app.get('/photos', async (req, res) => {
     const query = 'SELECT ID, NAME, WEIGHT, LENGTH, LATITUDE, LONGITUDE, USERNAME, UPDATED_AT, CREATED_AT FROM instant.PHOTOS;';
     const dbConnection = await mysql.createConnection(config).promise();
     const [rows] = await dbConnection.execute(query);
-    console.log("Result: " + JSON.stringify(rows));
-    res.status(200).send(rows);
+    res.status(200).json({
+        rows: rows
+    })
 })
 
-app.post('/upload', jsonParser, async (req, res) => {
-    console.log("req: " + JSON.stringify(req.body));
-    res.status(200).send('HI');
-})
+app.post('/upload',
+    jsonParser,
+    body("NAME").notEmpty().isString(),
+    body("WEIGHT").notEmpty().isNumeric(),
+    body("LENGTH").notEmpty().isNumeric(),
+    body("LATITUDE").notEmpty().isString(),
+    body("LONGITUDE").notEmpty().isString(),
+    body("USERNAME").notEmpty().isString(),
+    async (req, res) => {
+        const errors = validationResult(req);
 
-console.log("listening at 3000")
+        if (!errors.isEmpty()) {
+            return res.status(400).json({
+                success: false,
+                errors: errors.array()
+            });
+        }
+
+        res.status(200).json({
+            success: true,
+            message: 'Success',
+        })
+    });
