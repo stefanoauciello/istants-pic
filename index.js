aaf21863371571d928a4896879af93694f83d88b
auciellostefano1@gmail.com <auciellostefano1@gmail.com>
2021-03-20 18:59:00 +0100

    dockerized database add jsonParser for post

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
-const express = require('express')
+const express = require('express');
 const mysql = require('mysql2');
-const app = express()
+const bodyParser = require('body-parser');
+const app = express();
+
+const jsonParser = bodyParser.json()
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
+app.listen(3000)
 
 const config = {
     host: "127.0.0.1",
     port: 3306,
-    user: "instantuser",
-    password: "instantpassword"
+    user: "instant",
+    password: "instant"
 };
 
 app.get('/photos', async (req, res) => {
@@ -17,8 +22,9 @@ app.get('/photos', async (req, res) => {
     res.status(200).send(rows);
 })
 
-app.post('/upload-photo', async (req, res) => {
+app.post('/upload', jsonParser, async (req, res) => {
+    console.log("req: " + JSON.stringify(req.body));
     res.status(200).send('HI');
 })
 
-app.listen(3000)
+console.log("listening at 3000")
