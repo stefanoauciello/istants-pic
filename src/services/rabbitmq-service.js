597aa520870aee1deda9136a77b5fae4e720322b
auciellostefano1@gmail.com <auciellostefano1@gmail.com>
2021-03-21 19:49:10 +0100

    add try catch to rabbitmq

--- src/services/rabbitmq-service.js
+++ src/services/rabbitmq-service.js
@@ -6,34 +6,43 @@ const queue = 'instant';
 const routeKey = 'instant_route';
 
 async function sendNotify(pk) {
-    console.log("Publishing pk -> " + pk);
-    const connection = await amqplib.connect(amqp_url, "heartbeat=60");
-    const channel = await connection.createChannel()
-    await channel.assertExchange(exchange, 'direct', {durable: true}).catch(console.error);
-    await channel.assertQueue(queue, {durable: true});
-    await channel.bindQueue(queue, exchange, routeKey);
-    await channel.publish(exchange, routeKey, Buffer.from(pk.toString()));
-    setTimeout(() => {
-        channel.close();
-        connection.close();
-    }, 500);
+    try {
+        console.log("Publishing pk -> " + pk);
+        const connection = await amqplib.connect(amqp_url, "heartbeat=60");
+        const channel = await connection.createChannel()
+        await channel.assertExchange(exchange, 'direct', {durable: true}).catch(console.error);
+        await channel.assertQueue(queue, {durable: true});
+        await channel.bindQueue(queue, exchange, routeKey);
+        await channel.publish(exchange, routeKey, Buffer.from(pk.toString()));
+        setTimeout(() => {
+            channel.close();
+            connection.close();
+        }, 500);
+    } catch (e) {
+        throw new Error(e);
+    }
 }
 
+
 async function doConsume() {
-    const connection = await amqplib.connect(amqp_url, "heartbeat=60");
-    const channel = await connection.createChannel()
-    await connection.createChannel();
-    await channel.assertQueue(queue, {durable: true});
-    await channel.consume(queue, async (msg) => {
-        console.log("MESSAGE READED -> " + msg.content.toString());
-        await resizePhoto(msg.content.toString());
-        await channel.ack(msg);
-        await channel.cancel('myconsumer');
-    }, {consumerTag: 'myconsumer'});
-    setTimeout(() => {
-        channel.close();
-        connection.close();
-    }, 500);
+    try {
+        const connection = await amqplib.connect(amqp_url, "heartbeat=60");
+        const channel = await connection.createChannel()
+        await connection.createChannel();
+        await channel.assertQueue(queue, {durable: true});
+        await channel.consume(queue, async (msg) => {
+            console.log("MESSAGE READED -> " + msg.content.toString());
+            await resizePhoto(msg.content.toString());
+            await channel.ack(msg);
+            await channel.cancel('myconsumer');
+        }, {consumerTag: 'myconsumer'});
+        setTimeout(() => {
+            channel.close();
+            connection.close();
+        }, 500);
+    } catch (e) {
+        throw new Error(e);
+    }
 }
 
 module.exports = {sendNotify, doConsume};
