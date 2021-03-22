2985ce94b22938423a1ad3140544063c88a8f4cf
auciellostefano1@gmail.com <auciellostefano1@gmail.com>
2021-03-22 14:48:54 +0100

    refactor command for test up and down docker compose

--- test/index.test.js
+++ test/index.test.js
@@ -1,38 +1,41 @@
 const request = require('supertest');
+const mysql = require('mysql2');
 const app = require('../src/index');
 
-describe('Endpoints', () => {
-  const photo1 = {
-    NAME: 'PHOTO1',
-    WEIGHT: 10,
-    LENGTH: 10,
-    LATITUDE: '1O',
-    LONGITUDE: '15',
-    USERNAME: 'USER1',
-  };
+const config = {
+  host: '127.0.0.1',
+  port: 3306,
+  user: 'instant',
+  password: 'instant',
+};
 
-  const photo2 = {
-    NAME: 'PHOTO2',
-    WEIGHT: 10,
-    LENGTH: 10,
-    LATITUDE: '1O',
-    LONGITUDE: '15',
-    USERNAME: 'USER1',
-  };
-  it('should create a new photo', async () => {
-    const res1 = await request(app).post('/upload').send(photo1);
-    expect(res1.statusCode).toEqual(200);
-    expect(res1.body).toEqual({ message: 'Success', success: true });
-    const res2 = await request(app).post('/upload').send(photo2);
-    expect(res2.statusCode).toEqual(200);
-    expect(res2.body).toEqual({ message: 'Success', success: true });
+describe('Endpoints', () => {
+  beforeAll(async () => {
+    const dbConnection = await mysql.createConnection(config).promise();
+    await dbConnection.query('INSERT INTO instant.PHOTOS (NAME, WEIGHT, LENGTH, LATITUDE, LONGITUDE, USERNAME, CREATED_AT) VALUES(?, ?, ?, ?, ?, ?, ?)',
+      ['NAME', 'WEIGHT', 'LENGTH', 'LATITUDE', 'LONGITUDE', 'USERNAME', '2021-03-22 11:41:19']);
+    await dbConnection.query('INSERT INTO instant.PHOTOS (NAME, WEIGHT, LENGTH, LATITUDE, LONGITUDE, USERNAME, RESIZED, CREATED_AT) VALUES(?, ?, ?, ?, ?, ?, ?, ?)',
+      ['NAME', 'WEIGHT', 'LENGTH', 'LATITUDE', 'LONGITUDE', 'USERNAME', true, '2021-03-22 11:41:30']);
   });
+
+  afterAll(async () => {
+    const dbConnection = await mysql.createConnection(config).promise();
+    await dbConnection.query('TRUNCATE TABLE instant.PHOTOS;');
+  });
+
   it('should get all photos', async () => {
     const res = await request(app).get('/photos');
     expect(res.statusCode).toEqual(200);
-    expect(res.body.rows[0].ID).toEqual(1);
-    expect(res.body.rows[0].NAME).toEqual('PHOTO1');
-    expect(res.body.rows[1].ID).toEqual(2);
-    expect(res.body.rows[1].NAME).toEqual('PHOTO2');
+    expect(res.body.rows.length).toBe(2);
+    expect(res.body.rows[0].ID).toEqual(2);
+    expect(res.body.rows[0].NAME).toEqual('NAME');
+  });
+
+  it('should get all photos resized', async () => {
+    const res = await request(app).get('/resized-photos');
+    expect(res.statusCode).toEqual(200);
+    expect(res.body.rows.length).toBe(1);
+    expect(res.body.rows[0].ID).toEqual(2);
+    expect(res.body.rows[0].NAME).toEqual('NAME');
   });
 });
