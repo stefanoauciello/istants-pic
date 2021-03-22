a91559a549b107581f55f7b1aaddba762ba79df5
auciellostefano1@gmail.com <auciellostefano1@gmail.com>
2021-03-22 21:51:47 +0100

    refactor service, add test for upload and save photo, validation test

--- src/services/rabbitmq-service.js
+++ src/services/rabbitmq-service.js
@@ -1,14 +1,14 @@
 const amqplib = require('amqplib');
 const { resizePhoto } = require('./resizing-service');
 
-const amqp_url = 'amqp://localhost:5672';
+const amqpUrl = 'amqp://localhost:5672';
 const exchange = 'instant_exchange';
 const queue = 'instant';
 const routeKey = 'instant_route';
 
 async function sendNotify(pk) {
   console.log(`Publishing pk -> ${pk}`);
-  const connection = await amqplib.connect(amqp_url, 'heartbeat=60');
+  const connection = await amqplib.connect(amqpUrl, 'heartbeat=60');
   const channel = await connection.createChannel();
   await channel.assertExchange(exchange, 'direct', { durable: true }).catch(console.error);
   await channel.assertQueue(queue, { durable: true });
@@ -19,7 +19,7 @@ async function sendNotify(pk) {
 }
 
 async function doConsume() {
-  const connection = await amqplib.connect(amqp_url, 'heartbeat=60');
+  const connection = await amqplib.connect(amqpUrl, 'heartbeat=60');
   const channel = await connection.createChannel();
   await connection.createChannel();
   await channel.assertQueue(queue, { durable: true });
--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,7 @@
 const request = require('supertest');
 const mysql = require('mysql2');
+const fs = require('fs');
+const path = require('path');
 const app = require('../src/index');
 
 const config = {
@@ -11,11 +13,12 @@ const config = {
 
 describe('Endpoints', () => {
   beforeAll(async () => {
+    const rawData = fs.readFileSync(path.resolve(__dirname, 'assets/image.jpg'));
     const dbConnection = await mysql.createConnection(config).promise();
-    await dbConnection.query('INSERT INTO instant.PHOTOS (NAME, WEIGHT, LENGTH, LATITUDE, LONGITUDE, USERNAME, CREATED_AT) VALUES(?, ?, ?, ?, ?, ?, ?)',
-      ['NAME', 'WEIGHT', 'LENGTH', 'LATITUDE', 'LONGITUDE', 'USERNAME', '2021-03-22 11:41:19']);
-    await dbConnection.query('INSERT INTO instant.PHOTOS (NAME, WEIGHT, LENGTH, LATITUDE, LONGITUDE, USERNAME, RESIZED, CREATED_AT) VALUES(?, ?, ?, ?, ?, ?, ?, ?)',
-      ['NAME', 'WEIGHT', 'LENGTH', 'LATITUDE', 'LONGITUDE', 'USERNAME', true, '2021-03-22 11:41:30']);
+    await dbConnection.query('INSERT INTO instant.PHOTOS (NAME, WEIGHT, LENGTH, LATITUDE, LONGITUDE, USERNAME, PHOTO, CREATED_AT) VALUES(?, ?, ?, ?, ?, ?, ?, ?)',
+      ['NAME', 'WEIGHT', 'LENGTH', 'LATITUDE', 'LONGITUDE', 'USERNAME', rawData, '2021-03-22 11:41:19']);
+    await dbConnection.query('INSERT INTO instant.PHOTOS (NAME, WEIGHT, LENGTH, LATITUDE, LONGITUDE, USERNAME, RESIZED, PHOTO, CREATED_AT) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)',
+      ['NAME', 'WEIGHT', 'LENGTH', 'LATITUDE', 'LONGITUDE', 'USERNAME', true, rawData, '2021-03-22 11:41:30']);
   });
 
   afterAll(async () => {
@@ -37,5 +40,31 @@ describe('Endpoints', () => {
     expect(res.body.rows.length).toBe(1);
     expect(res.body.rows[0].ID).toEqual(2);
     expect(res.body.rows[0].NAME).toEqual('NAME');
+  });
+
+  it('should upload photo', async () => {
+    const res = await request(app).post('/upload').set('Content-Type', 'multipart/form-data')
+      .field('NAME', 'prova')
+      .field('WEIGHT', '10.5')
+      .field('LENGTH', '10')
+      .field('LATITUDE', 'A')
+      .field('LONGITUDE', 'B')
+      .field('USERNAME', 'USER1')
+      .attach('IMAGE', path.resolve(__dirname, 'assets/image.jpg'));
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toEqual({ message: 'Success', success: true });
+  });
+
+  it('should return validation error', async () => {
+    const res = await request(app).post('/upload').set('Content-Type', 'multipart/form-data')
+      .field('NAME', 'prova')
+      .field('WEIGHT', '10.5')
+      .field('LENGTH', 'a')
+      .field('LATITUDE', 'A')
+      .field('LONGITUDE', 'B')
+      .field('USERNAME', 'USER1')
+      .attach('IMAGE', path.resolve(__dirname, 'assets/image.jpg'));
+    expect(res.statusCode).toEqual(400);
+    expect(res.body).toEqual({ errors: 'fields or files not valid', success: false });
   });
 });
